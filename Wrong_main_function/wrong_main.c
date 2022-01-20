4fceb3a0518de043ecf983ed0ca48873d3513603
ddxxdd-code <65443542+ddxxdd-code@users.noreply.github.com>
2022-01-20 14:57:11 -0500

    Create wrong_main.c

---
+++ Wrong_main_function/wrong_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+ return 0; 
+}
+/*
+And this:
+int main(char *argv[], int argc) {
+  return 0;
+}
+*/
+/*
+the second one will not work as the variables give to the program is in different type.
+*/
